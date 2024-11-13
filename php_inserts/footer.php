ead0c1ccce996adefc34d707fb1e0217ae492330
Lena_Marina <if24b255@technikum-wien.at>
2024-11-13 11:48:08 +0100

    lol

--- php_inserts/footer.php
+++ php_inserts/footer.php
@@ -5,7 +5,7 @@ USAGE: no special usage
 */
 
 echo '
-<footer class="col-12">
+<footer class="col-12 footer">
     <a class="footer" href="impressum.php">Impressum</a>
     <a class=" footer" href="faq.php">FAQs</a>
 </footer>
